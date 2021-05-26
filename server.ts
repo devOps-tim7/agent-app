a23dd48eb88dd4cd2f21e2204925d690343ed3a7
Marko131 <markostanic222@gmail.com>
2021-05-26 21:21:21 +0200

    Add limit for uploading files

--- server.ts
+++ server.ts
@@ -5,7 +5,8 @@ import ProductRoute from './src/routes/Products';
 
 export const createServer = () => {
   const app = express();
-  app.use(express.json());
+  app.use(express);
+  app.use(express.json({ limit: '25mb' }));
   app.use('/api/product', ProductRoute);
   app.use(errorHandler);
   return app;
