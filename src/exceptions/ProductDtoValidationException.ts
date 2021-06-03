bc11df8bcaf078737a9b43ef9281af5652d8505b
m-milovanovic <m.milovanovic@outlook.com>
2021-06-03 21:58:47 +0200

    maybe this will work

--- src/exceptions/ProductDtoValidationException.ts
+++
@@ -1,8 +0,0 @@
-import HttpException from './HttpException';
-import PropertyError from './PropertyError';
-
-export default class ProductDtoValidationException extends HttpException {
-  constructor(errors: PropertyError[]) {
-    super(400, errors);
-  }
-}
--- src/helpers/SetupTests.ts
+++
@@ -1,9 +0,0 @@
-import connection from '../helpers/Connection';
-
-global.beforeAll(async () => {
-  await connection.create();
-});
-
-global.afterAll(async () => {
-  await connection.close();
-});
