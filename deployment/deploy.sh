b9fec1ed0cf4928dca9428897851568482d07953
m-milovanovic <m.milovanovic@outlook.com>
2021-06-13 14:08:15 +0200

    fix the step in .yml

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -11,20 +11,21 @@ case $ALL_HEROKU_APPS in (*"$TERRAFORM_PG_BACKEND"*)
    ;;
 esac
 
-rm -rf ./terraform/gateway/Dockerfile
-rm -rf ./terraform/products/Dockerfile
-rm -rf ./terraform/purchases/Dockerfile
-
-echo "FROM $GATEWAY_IMAGE" >> ./terraform/gateway/Dockerfile
-cat ./terraform/gateway/Dockerfile
-
-echo "FROM $PRODUCTS_IMAGE" >> ./terraform/products/Dockerfile
-cat ./terraform/gateway/Dockerfile
-
-echo "FROM $PURCHASES_IMAGE" >> ./terraform/purchases/Dockerfile
-cat ./terraform/gateway/Dockerfile
-
 cd terraform || exit
+
+rm -rf ./gateway/Dockerfile
+rm -rf ./products/Dockerfile
+rm -rf ./purchases/Dockerfile
+
+echo "FROM $GATEWAY_IMAGE" >> ./gateway/Dockerfile
+cat ./gateway/Dockerfile
+
+echo "FROM $PRODUCTS_IMAGE" >> ./products/Dockerfile
+cat ./gateway/Dockerfile
+
+echo "FROM $PURCHASES_IMAGE" >> ./purchases/Dockerfile
+cat ./gateway/Dockerfile
+
 DATABASE_URL=$(heroku config:get DATABASE_URL --app "$TERRAFORM_PG_BACKEND") && export DATABASE_URL
 terraform init -backend-config="conn_str=$DATABASE_URL"
 terraform apply -auto-approve -var stage=${STAGE}
